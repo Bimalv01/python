ec1939bce87034bff8b9c2c0afe9d66f60533a50
Bimalv01 <96819738+Bimalv01@users.noreply.github.com>
2024-01-24 11:08:13 +0530

    Update area.py

--- area.py
+++ area.py
@@ -12,30 +12,7 @@ cercum()
 print(area())
 
 
-l=[5,2,8,1,6,9,3]
-t=[]
-for i in l:
-    if i%2==0:
-        print(i)
-        t.append(i)
-print(sum(t))
 
-s=["apple","banana","cherry","date","elder berry"]
-
-a=[]
-for i in s:
-    li=len(i)
-    a.append(li)
-x=a.index(max(a))
-print(s[x])
-
-
-l=[]
-for i in range(1,5):
-    p=int(input(f"enter {i} "))
-    l.append(p)
-
-print(max(l))    
 
 
         
